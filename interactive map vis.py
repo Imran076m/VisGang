e4e0f6363f55291a0f9e81f79a7f74787827552b
Imran <i.makan@student.tue.nl>
2022-01-20 19:03:54 +0100

    Density map final

--- interactive map vis.py
+++ interactive map vis.py
@@ -7,12 +7,16 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv(r"C:\Users\20203572\Downloads\\Accidents-2009.csv")
+df = pd.read_csv(r"C:\Users\20203572\Downloads\\Accidents-2007-2008.csv")
 
 df = df[df.latitude != '?']
 df = df[df.longitude != '?']
 df['Latitude'] = pd.to_numeric(df['latitude'])
 df['Longitude'] = pd.to_numeric(df['longitude'])
+df8 = df[df['accident_year'] == 2008] 
+df7 = df[df['accident_year'] == 2007] 
+df10 = df[df['accident_year'] == 2010] 
+df11 = df[df['accident_year'] == 2011] 
 
 
 
@@ -22,22 +26,15 @@ import plotly.graph_objects as go
 
 dfs = df
 import plotly.express as px
-fig = px.density_mapbox(df, lat='Latitude', lon='Longitude', z='number_of_casualties', radius=3,
-                        center=dict(lat=54.5, lon=-3.943646), zoom=4.2, range_color=(0, 20),
+fig = px.density_mapbox(df7, lat='Latitude', lon='Longitude', z='number_of_casualties', radius=2,
+                        center=dict(lat=54.5, lon=-3.943646), zoom=4, range_color=(0, 60),
                         mapbox_style="stamen-terrain")
 
-fig.update_layout(
-        title = 'Accidents in 2009',
-        geo_scope='europe', 
-    )
+fig.update_traces(legendgrouptitle_text='Number of Casualties')
 
-
-
-fig.update_layout(width=600, height=400, margin={"r":0,"t":0,"l":0,"b":0})
-
-fig.update_layout(
+fig.update_layout(title = 'Number of Casualties',  geo_scope='europe', width=600, height=400, margin={"r":10,"t":50,"l":10,"b":0}, 
         geo = dict(
-            projection_scale=8, 
+            projection_scale=5, 
             center=dict(lat=54.5, lon=-3.943646),
         ))
 
@@ -48,5 +45,6 @@ app = dash.Dash()
 app.layout = html.Div([
     dcc.Graph(figure=fig)
 ])
+
 
 app.run_server(debug=True, use_reloader=False) 
